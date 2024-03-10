702bda0ba2d908cfd9d0a5c8157a1ab18a03c79d
AleksMitushev <147534345+AleksMitushev@users.noreply.github.com>
2024-03-10 14:21:20 +0200

    This is test commit message

---
+++ week01/task02.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n, count;
+    cout << "Enter a number";
+    cin >> n;
+    cout << "Prime number till" << n << endl;
+
+    for(int i=2; i<=n; i++) 
+    {
+        count = 0;
+        for(int j=2; j<i; j++)
+        {
+            if (i%j==0)
+            count++;
+        }
+        if(count == 0)
+        {
+            cout << i << endl;
+
+        } 
+    }
+    return 0;
+
+}
+
